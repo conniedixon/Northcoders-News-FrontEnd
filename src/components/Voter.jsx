235140d015583ccaf69390010448890841e7c81d
connie <conniedixon106@gmail.com>
2020-03-03 14:54:46 +0000

    Created framework of voter component

---
+++ src/components/Voter.jsx
@@ -0,0 +1,18 @@
+/** @format */
+
+import React, { Component } from 'react';
+
+class Voter extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <button>Upvote</button>
+        <p>{}</p>
+        <button>Downvote</button>
+      </div>
+    );
+  }
+}
+
+export default Voter;
