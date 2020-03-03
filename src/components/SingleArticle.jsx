8a9a3050383757f7dbd6b5d7d03c4d74dc21535b
connie <conniedixon106@gmail.com>
2020-03-03 08:48:14 +0000

    Added single article framework

---
+++ src/components/SingleArticle.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const SingleArticle = ({article}) => {
+    return (
+        <div>
+            <h3>{article.title}</h3>
+            <h6>n/{article.topic} - posted by {article.author} on {article.created_at}</h6>
+            <p>{article.body}</p>
+            <br></br>
+            <h5>Comments</h5>
+            <p>{article.comment_count} comments</p>
+            <p>{article.votes}</p><button>Upvote</button><button>Downvote</button>
+            <form>
+                //post a comment here
+            </form>
+                //article comments here
+        </div>
+    );
+};
+
+export default SingleArticle;
