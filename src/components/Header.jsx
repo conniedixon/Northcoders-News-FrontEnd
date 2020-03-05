f7206d41988663ae4deb5e9e5a3105e00227ec6e
connie <conniedixon106@gmail.com>
2020-03-05 10:44:51 +0000

    Fixed header CSS to be responsive!

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,7 @@ import React from 'react';
 const Header = () => {
   return (
     <header className='header'>
-      <h1 className='NCtags'> {`<`} </h1>
-      <h1>Northcoders News </h1> <h1 className='NCtags'> {` />`} </h1>
+      <h1>{`<`}Northcoders News </h1>
     </header>
   );
 };
